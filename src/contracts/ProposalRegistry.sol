6ce80adcd81082f9de2118be0bbf2115751565ab
Dristpunk <dristpunk@gmail.com>
2022-09-20 13:04:57 +0300

    add execute

--- src/contracts/ProposalRegistry.sol
+++ src/contracts/ProposalRegistry.sol
@@ -22,6 +22,7 @@ struct Proposal {
     Status status;
     Transaction[] pipeline;
     uint256 creationBlock;
+    uint256 creationTime;
     uint256 yesCount;
     uint256 noCount;
 }
@@ -38,18 +39,21 @@ contract ProposalRegistry {
 
     mapping(uint256 => Proposal) public proposals;
     mapping(address => mapping(uint256 => VoteType)) public voted;
-    IVotingToken votingToken;
+    IVotingToken public votingToken;
+    uint256 public proposalExpirationTime;
 
 
-    constructor(IVotingToken _token) {
+    constructor(IVotingToken _token, uint256 _proposalExpirationTime) {
         votingToken = _token;
+        proposalExpirationTime = _proposalExpirationTime;
+        
     }
     function createProposal(uint256 _propId, Transaction[] calldata _pipeline) external {
         require(proposals[_propId].status == Status.NONE, 'Proposal with this ID already exists');
         
         _beforeCreateProposal(_propId, _pipeline);
 
-        proposals[_propId] = Proposal(Status.EXISTS, _pipeline, block.number, 0, 0);
+        proposals[_propId] = Proposal(Status.EXISTS, _pipeline, block.number, block.timestamp, 0, 0);
 
         _afterCreateProposal(_propId, _pipeline);
     }
@@ -59,6 +63,7 @@ contract ProposalRegistry {
 
 
     function vote(uint256 _propId, bool _decision, bytes calldata _data) external {
+        require(!proposalExpired(_propId), 'Proposal expired');
         require(proposals[_propId].status == Status.EXISTS, 'Proposal must exist');
 
         Proposal storage proposal = proposals[_propId];
@@ -88,5 +93,24 @@ contract ProposalRegistry {
 
     function _processVoteData(uint256 _propId, bool _decision, bytes calldata _data) internal virtual {}
 
+    function execute(uint256 _propId) external {
+        require(!proposalExpired(_propId), 'Proposal expired');
+        require(proposals[_propId].status == Status.ACCEPTED, 'Proposal must be accepted');
+
+        Proposal storage proposal = proposals[_propId];
+
+        proposal.status = Status.EXECUTED;
+
+        for(uint256 i=0; i < proposal.pipeline.length; ++i) {
+            Transaction memory trans = proposal.pipeline[i];
+            (bool success_, bytes memory data_) = trans.to.call{value: trans.value}(trans.data);
+            // TODO: сделать что-то с data_
+            require(success_, 'Transaction failed');
+        }
+    }
+
+    function proposalExpired(uint256 _propId) public view returns(bool) {
+        return proposals[_propId].creationTime + proposalExpirationTime > block.timestamp;
+    }
 
 }
