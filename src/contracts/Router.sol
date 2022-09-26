ec2ace97f779e0521412bdbfcaf72d19c461728d
Dristpunk <dristpunk@gmail.com>
2022-09-27 01:29:01 +0300

    add ERC165 to Router

--- src/contracts/Router.sol
+++ src/contracts/Router.sol
@@ -6,8 +6,17 @@ pragma solidity ^0.8.0;
 import "../interfaces/IRouter.sol";
 import "../interfaces/IProposalRegistry.sol";
 import {Proposal, Transaction} from "./ProposalRegistry.sol";
+import "openzeppelin/utils/introspection/ERC165.sol";
 
-abstract contract Router is IRouter {
+
+abstract contract Router is ERC165, IRouter {
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+        return
+            interfaceId == type(IRouter).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+    
     function onVote (
         uint256 _propId, 
         uint256 _transId, 
--- src/interfaces/IRouter.sol
+++ src/interfaces/IRouter.sol
@@ -2,7 +2,9 @@
 
 pragma solidity 0.8.17;
 
-interface IRouter {
+import "openzeppelin/utils/introspection/IERC165.sol";
+
+interface IRouter is IERC165 {
     function onVote(
         uint256 propId,
         uint256 transId,  // id of Transaction related to current IRouter in pipeline
