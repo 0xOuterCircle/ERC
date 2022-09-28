8620b48ebaafc3b272c1637f8a65b87a902efceb
Dristpunk <dristpunk@gmail.com>
2022-09-28 17:45:51 +0300

    add getUiReport func and textProposal func

--- src/contracts/Router.sol
+++ src/contracts/Router.sol
@@ -2,28 +2,21 @@
 
 pragma solidity ^0.8.0;
 
-
 import "../interfaces/IRouter.sol";
 import "../interfaces/IProposalRegistry.sol";
 import {Proposal, Transaction} from "./ProposalRegistry.sol";
 import "openzeppelin/utils/introspection/ERC165.sol";
 
-
 abstract contract Router is ERC165, IRouter {
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
-        return
-            interfaceId == type(IRouter).interfaceId ||
-            super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view virtual override (ERC165, IERC165) returns (bool) {
+        return interfaceId == type(IRouter).interfaceId || super.supportsInterface(interfaceId);
     }
-    
-    function onVote (
-        uint256 _propId, 
-        uint256 _transId, 
-        bool _vote, 
-        bytes calldata _voteData
-        ) external virtual returns(bytes memory) {
 
+    function onVote(uint256 _propId, uint256 _transId, bool _vote, bytes calldata _voteData)
+        external
+        virtual
+        returns (bytes memory)
+    {
         Proposal memory prop = IProposalRegistry(msg.sender).getProposal(_propId);
         Transaction memory trans = prop.pipeline[_transId];
 
@@ -31,11 +24,26 @@ abstract contract Router is ERC165, IRouter {
         return transData;
     }
 
-    function _processVote(
-        Proposal memory prop, 
-        Transaction memory trans, 
-        bool _vote, bytes calldata 
-        _voteData
-        ) internal virtual returns(bytes memory);
+    function _processVote(Proposal memory prop, Transaction memory trans, bool _vote, bytes calldata _voteData)
+        internal
+        virtual
+        returns (bytes memory);
 
+    /**
+     * @param text Any text to vote for: link, message, id, etc.
+     */
+    function textProposal(string calldata text) external pure returns (string calldata) {
+        return text;
+    }
+
+    // =================== UI Report to frontent purposes ===================
+
+    /**
+     * @dev Report format: <funcName>::[comma-separated user argument names]\n
+     * @dev Example: myFunc::[arg1,arg2]\n
+     */
+    function getUiReport() external pure returns (string memory report) {
+        report = string.concat(report, "textProposal", "::", "[]", "\n");
+        // report = string.concat(report, "anotherFunc", "::", "[arg1, arg2]", "\n");
+    }
 }
